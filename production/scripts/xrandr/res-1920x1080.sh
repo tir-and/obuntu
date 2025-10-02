23a5d47216cb116becb81fd5931830f8f8212724
TirAnd <tiron.andric@gmail.com>
2025-10-03 00:18:46 +0200

    Delete production directory
    Reorg

--- production/scripts/xrandr/res-1920x1080.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-OUTPUT=$(xrandr --query | awk '/ connected/{print $1; exit}')
-xrandr --output "$OUTPUT" --mode 1920x1080 --rate 60
--- production/scripts/xrandr/res-3440x1440.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-OUTPUT=$(xrandr --query | awk '/ connected/{print $1; exit}')
-xrandr --output "$OUTPUT" --mode 3440x1440 --rate 60
