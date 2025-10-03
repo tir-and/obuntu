fb235c3216a541186b3cdabe097390b3197f359c
TirAnd <tiron.andric@gmail.com>
2025-10-03 22:15:51 +0200

    Update openbox-deploy.sh

--- scripts/openbox-deploy.sh
+++ scripts/openbox-deploy.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash\nset -euo pipefail\nREPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"\n\n#!/usr/bin/env bash
 set -euo pipefail
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
