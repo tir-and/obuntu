3d96a8edac738bb751419cf8269c8be1d7c53a6c
TirAnd <tiron.andric@gmail.com>
2025-10-14 11:18:19 +0200

    Update sestup-ubuntu-host.sh

--- sestup-ubuntu-host.sh
+++ sestup-ubuntu-host.sh
@@ -18,10 +18,10 @@ DEPLOY_REPO_CONFIGS=${DEPLOY_REPO_CONFIGS:-1}    # copy configs/ from this repo
 
 REPO_URL="${REPO_URL:-https://github.com/tir-and/ubuntu-qemu-host}"
 
-say(){ printf "\033[1;34m[INFO]\033[0m %s\n" "$*"; }
-ok(){  printf "\033[1;32m[OK]\033[0m %s\n" "$*"; }
-warn(){printf "\033[1;33m[WARN]\033[0m %s\n" "$*"; }
-need_root(){ [[ $EUID -eq 0 ]] || { echo "Run as root: sudo $0"; exit 1; }; }
+say() { printf "\033[1;34m[INFO]\033[0m %s\n" "$*"; }
+ok() {  printf "\033[1;32m[OK]\033[0m %s\n" "$*"; }
+warn() {printf "\033[1;33m[WARN]\033[0m %s\n" "$*"; }
+need_root() { [[ $EUID -eq 0 ]] || { echo "Run as root: sudo $0"; exit 1; }; }
 
 need_root
 say "Updating APT…"
