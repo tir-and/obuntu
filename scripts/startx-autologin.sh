a57cd78511a6ef6b5060218d50e55d5c7e8d7cba
TirAnd <tiron.andric@gmail.com>
2025-10-14 09:50:09 +0200

    Delete scripts/startx-autologin.sh

--- scripts/startx-autologin.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash\nset -euo pipefail\nREPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"\n\n#!/usr/bin/env bash
-if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
-    exec startx
-fi
